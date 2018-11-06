de4bb98e13acd8a88442f4a5dd7d81f1b12fff29
acheng-ubc <42976545+acheng-ubc@users.noreply.github.com>
2018-11-06 11:12:40 -0800

    copying local files to participation repo

---
+++ powers/R/pow.R
@@ -0,0 +1,11 @@
+#' Calculate a power of x
+#'
+#' Default calculates the square of a number x
+#'
+#' @param x real number
+#' @param a real number
+#'
+#' @return real number
+pow <- function(x, a=2){
+  x^a
+}
---
+++ powers/R/square.R
@@ -0,0 +1,7 @@
+#' Square one number#'
+#'
+#' @param x
+#'
+#' @return x squared
+#' @export
+square <- pow(x)
